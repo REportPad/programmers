2dab8416002dcc744af8cd33aa7682111ae8bb6f
jehyun <ggurggur123@naver.com>
2020-12-26 11:37:04 +0900

    Update PhoneNumberList.cpp

--- PhoneNumberList.cpp
+++ PhoneNumberList.cpp
@@ -1,49 +1,20 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 #include <iostream>
-
 using namespace std;
-typedef vector<int> vint;
 
-bool solution(vector<string> phone_book) {
-    int n = phone_book.size();
-    vint small;
-    vint smallIdx;
-    small.push_back(21);
-    smallIdx.push_back(-1);
-    int k=0;
-    int cnt = 1;
-    
-    for(int i=0; i < n; ++i){
-        int temp = phone_book[i].size();
-        if(small.back() > temp){
-            small[0] = temp;
-            smallIdx[0] = i;
-        }
-        else if(small.back() == temp){
-            small.push_back(temp);
-            smallIdx.push_back(i);
-            ++cnt;
-        }
-    }
-    
-    
-    //
-    for(int k=0; k < cnt; ++k){
-        for(int i=0; i < n; ++i){
-            if(i == smallIdx[k]) continue;
-            string p = phone_book[smallIdx[k]];
-            string temp = "";
-                
-            for(int j=0; j < p.size(); ++j){
-                temp += phone_book[i][j];
-            }
-        
-            if(temp == p) 
-                return false;
+bool solution(vector<string> phoneBook) {
+    bool answer = true;
+
+    sort(phoneBook.begin(), phoneBook.end());
+
+    for ( int i = 0 ; i < phoneBook.size() - 1 ; i++ ){
+        if ( phoneBook[i] == phoneBook[i+1].substr(0, phoneBook[i].size()) ){
+            answer = false;
+            break;
         }
     }
 
-    
-    return true;
+    return answer;
 }
